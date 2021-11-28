309e38a20b1aa180dd5eeef4c5839eb2cc790fb3
Boubourazi <aurelien.llorca@gmail.com>
2021-11-28 17:51:04 +0100

    add ts types

--- csl_project/src/app.ts
+++ csl_project/src/app.ts
@@ -1,13 +1,10 @@
 import express from 'express';
 import fileUpload from 'express-fileupload';
 import cors from 'cors';
-import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import _ from 'lodash';
 import bpmnRouter from "./routes/bpmnRouter";
 
-import { CompleterResult } from "readline";
-import path from "path/win32";
 
 const app = express();
 const port = 8080; // default port to listen
