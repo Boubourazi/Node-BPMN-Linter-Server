c39c1363717ca6e31aa36e6060f95d7bc6c27349
Boubourazi <aurelien.llorca@gmail.com>
2021-12-07 13:50:16 +0100

    stable simple renderer

--- csl_project/src/logic/Renderer.ts
+++ csl_project/src/logic/Renderer.ts
@@ -8,16 +8,18 @@ export class Renderer {
     }
     
     public async render(bpmn?: string, lint?:any):Promise<File> {
-        const browser = await puppeteer.launch({headless: false, slowMo: 250});
+        const browser = await puppeteer.launch({headless: true});
         const page = await browser.newPage();
         await page.setContent(`<div id="canvas"></div>`);
         await page.addScriptTag({url: 'https://unpkg.com/bpmn-js@8.8.3/dist/bpmn-viewer.development.js'});
-        const t = await page.evaluate(`
-            const viewer = new BpmnJS({container:'#canvas'});
-            await viewer.importXML(${bpmn});
-            viewer.get('canvas').zoom('fit-viewport');
+        const t = await page.evaluate(`(async () => {
+                const viewer = new BpmnJS({container:'#canvas'});
+                await viewer.importXML(\`${bpmn.toString()}\`);
+                viewer.get('canvas').zoom('fit-viewport');
+            })();
         `);
-        console.log(t);
+        await page.screenshot({ path: './screenshots/example.png' });
+        console.log("screen done");
         //
         //await page.setContent(bpmn);
         await browser.close();
